a830312ebb327fe9a1b0882e12f3feedc2cbc979
Tom Lynch <twhlynch@gmail.com>
2025-04-11 12:32:17 +1000

    add fog to particles

--- shaders.js
+++ shaders.js
@@ -2,19 +2,29 @@ export const particleVS = /* glsl */`
 	attribute vec3 color;
 	varying vec3 vColor;
 
+	varying float vDistance;
+
 	void main() 
 	{
 		vColor = color;
 
+		vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+		vDistance = length(mvPosition.xyz);
+
 		gl_PointSize = 1.0;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+		gl_Position = projectionMatrix * mvPosition;
 	}
 `
 export const particleFS = /* glsl */`
 	varying vec3 vColor;
+	varying float vDistance;
 
 	void main()
 	{
-		gl_FragColor = vec4(vColor, 1.0);
+		vec4 color = vec4(vColor, 1.0);
+		float fogAmount = clamp(vDistance * 0.03, 0.0, 1.0);
+		color.rgb = mix(color.rgb, vec3(0.0), fogAmount);
+
+		gl_FragColor = color;
 	}
 `
